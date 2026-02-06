7a74be8a05fcf9f372e96c24857b19062ad2ca72
nschool3424 <nbsinclair7@gmail.com>
2026-02-06 11:58:52 -0600

    More Days, updated Day Maker

--- day_maker.py
+++ day_maker.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 print('Enter today\'s number:')
@@ -13,10 +14,24 @@ try:
     print(f'Folder "{folder}" created')
     fname = folder + "/description.txt"
     with open(fname,'w') as f_desc:
-        print('Enter the problem link:')
-        link = input()
+        # print('Enter the problem link:')
+        # link = input()
+
+        # title_lower = title.lower()
+        # title_no_hyphens = title_lower.replace('-','')
+        # title_arr = re.split(r'\s+',title_no_hyphens)
+        # title_hyphens = '-'.join(title_arr)
+
+        title_hyphens = '-'.join(
+            re.split(r'\s+',
+                title.lower().
+                    replace('-','') ) )
+
+
+        link = f'https://www.hackerrank.com/challenges/30-{title_hyphens}/problem'
         f_desc.write(link)
-    fname = folder + "/Solution.Java"
+        print(f'Link assumed to be "{link}". Please double check this link')
+    fname = folder + '/Solution.Java'
     f_soln = open(fname,'x')
     
 except FileExistsError:
